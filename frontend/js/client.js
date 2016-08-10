5b208e892c7c0f52c3b6562070917fe664114829
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-08-10 16:17:31 -0400

    work on redirects

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -436,13 +436,13 @@ app.controller('CompleteController', function($cookies, $http, $scope, $location
     // if Code Challenge completed, redirect
     if (data.codeChallengeCompleted) {
       $location.path('/schedule');
-    } else if (data.pageLastCompleted !== 4 && !data.applicationCompleted) {
+    } else if (data.pageLastCompleted !== 4) {
       $location.path('/page2');
     }
 
     // if the user ended back on the complete page after an email
     // has already been sent, don't send the email again
-    if (!data.applicationCompleted) {
+    if (!data.applicationCompleted && data.pageLastCompleted === 4) {
       // call backend to send the email
       $http.post(API + '/complete', { data: userToken })
         .then(function(response) {
