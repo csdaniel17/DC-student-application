e26ee7fda38faf20d0cd6b5e5750e3bb66741407
Kyle Luck <luck.kyle@gmail.com>
2016-08-12 10:42:33 -0400

    postMessage working

--- codeTest/jasmine/lib/jasmine-2.4.1/reporter.js
+++ codeTest/jasmine/lib/jasmine-2.4.1/reporter.js
@@ -1,47 +1,51 @@
+var passed = [];
+var failed = [];
+
 var myReporter = {
-  jasmineStarted: function(suiteInfo) {
-
-    //console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
-  },
-
-  suiteStarted: function(result) {
-
-    //console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
-  },
-
-  specStarted: function(result) {
-
-    //console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
-  },
-
   specDone: function(result) {
 
-    console.log('Spec: ' + result.description + ' was ' + result.status);
-    for(var i = 0; i < result.failedExpectations.length; i++) {
-
-      console.log('Failure: ' + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
+    // push to appropriate array
+    if (result.status === 'passed') {
+      passed.push(result.id);
+    } else {
+      failed.push(result.id);
     }
-
-
-    console.log(result.passedExpectations.length);
+    // console.log('result in specDone: ', result);
+    // console.log('Spec: ' + result.description + ' was ' + result.status);
+    // for(var i = 0; i < result.failedExpectations.length; i++) {
+    //
+    //   console.log('Failure: ' + result.failedExpectations[i].message);
+    //   console.log(result.failedExpectations[i].stack);
+    // }
+    //
+    //
+    // console.log(result.passedExpectations.length);
   },
 
 
 
   suiteDone: function(result) {
 
-    console.log('Suite: ' + result.description + ' was ' + result.status);
-    for(var i = 0; i < result.failedExpectations.length; i++) {
+    var data = {
+      passed: passed,
+      failed: failed
+    };
+    console.log('data is: ', data);
+    parent.postMessage(data, "*");
 
-      console.log('AfterAll ' + result.failedExpectations[i].message);
-      console.log(result.failedExpectations[i].stack);
-    }
+    // console.log('Suite: ' + result.description + ' was ' + result.status);
+    // console.log("length is: ", result.failedExpectations.length);
+    // console.log('result is', result);
+    // for(var i = 0; i < result.failedExpectations.length; i++) {
+    //
+    //   console.log('AfterAll ' + result.failedExpectations[i].message);
+    //   console.log(result.failedExpectations[i].stack);
+    // }
   },
 
 
   jasmineDone: function() {
-    console.log('Finished suite');
+    //console.log('Finished suite');
   }
 };
 
