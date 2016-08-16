e83c1f1f6bbd7a04fa7d06a2ff8e77c5cd183e90
Kyle Luck <luck.kyle@gmail.com>
2016-08-16 10:29:40 -0400

    filter by appCompleteDate on admin dashboard

--- frontend/js/controllers/adminController.js
+++ frontend/js/controllers/adminController.js
@@ -16,30 +16,8 @@ app.controller('AdminController', function($scope, $cookies, $rootScope, $locati
       $scope.numScheduled = adminData.numScheduled;
 
       $scope.labels = ["App in progress", "At Challenge", "At Interview", "Finished"];
-      // $scope.series = ['Series A', 'Series B'];
       $scope.data = [adminData.numInProgress, adminData.numAtChallenge, adminData.numAtInterview, adminData.numScheduled];
-      // $scope.onClick = function (points, evt) {
-      //   console.log(points, evt);
-      // };
-      // $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-      // $scope.options = {
-      //   scales: {
-      //     yAxes: [
-      //       {
-      //         id: 'y-axis-1',
-      //         type: 'linear',
-      //         display: true,
-      //         position: 'left'
-      //       },
-      //       {
-      //         id: 'y-axis-2',
-      //         type: 'linear',
-      //         display: true,
-      //         position: 'right'
-      //       }
-      //     ]
-      //   }
-      // };
+
     })
     .catch(function(err) {
       if ($rootScope.fromPage !== 'admin') {
@@ -49,4 +27,18 @@ app.controller('AdminController', function($scope, $cookies, $rootScope, $locati
       }
     });
 
+  $scope.dateRangeFilter = function(startDate, endDate) {
+
+
+    return function (item) {
+      if (item.applicationCompletedDate === null) {
+        return false;
+      }
+      if (new Date(item.applicationCompletedDate) >= new Date(startDate) &&
+          new Date(item.applicationCompletedDate) <= new Date(endDate)) {
+        return true;
+      }
+      return false;
+    }
+  }
 });
