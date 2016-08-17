f6e69dfbeaec8d0bc4f3aa4d4dc33bc55909a380
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-08-17 10:40:52 -0400

    update code challenge styling

--- frontend/js/controllers/codeController.js
+++ frontend/js/controllers/codeController.js
@@ -22,67 +22,50 @@ app.controller('CodeController', function($scope, $http, $timeout, $cookies, $lo
     _editor.$blockScrolling = Infinity;
 
     //set content of editor:
-    var initialCode = `
-//////////////////////////////////////////////////////////////////
-///////////////DIGITALCRAFTS ADMISSIONS CHALLENGE/////////////////
-//////////////////////////////////////////////////////////////////
-// INSTRUCTIONS:
-// - Code is to be written in JavaScript only. You won't need HTML or CSS.
-// - Please write the code for each question directly below the question itself.
-// - Hit Run at the bottom of the editor to test your code and execute any functions you've called.
-// - Press "Save & Continue" when finished.
-// - For this challenge, consider the words "argument" and "parameter" to be interchangable.
-// - All variables and methods are persistent from question to question, so there is no need to redeclare anything!
-// - PLEASE NOTE: You should also take a screenshot of your work (or copy it elsewhere) just in case!!
-// - Please use Chrome!
-// - You may use console.log to test/debug your work. The result of a console.log() call will display below the code editor.
-//    - Example: console.log(yourFunction());
-
-//////////////////////////PLEASE READ INSTRUCTIONS VERY CAREFULLY!//////////////////////////
-
-// 1) Declare two variables, a string and an integer named "fullName" and "yearBorn". Set them equal to Linus Torvalds's name and the year he was born.
+    var initialCode = `/*
+1) Declare two variables, a string and an integer named "fullName" and "yearBorn". Set them equal to Linus Torvalds's name and the year he was born.
+*/
 
 
 
 
-// 2) Declare an empty array called "myArray".
-// Add the variables from the question above (fullName and yearBorn) to the empty array using the push method.
-// Print to the console.
+/*
+2) Declare an empty array called "myArray". Add the variables from the question above (fullName and yearBorn) to the empty array using the push method. Print to the console to test your result.
+*/
 
 
 
 
-// 3) Write a simple function called "sayHello" that takes no parameters.
-// Make it return "Hello!".
-// Call the function.
+/*
+3) Write a simple function called "sayHello" that takes no parameters. Make it return "Hello!". Call the function.
+*/
 
 
 
 
-// 4) Declare a variable named splitName and set it equal to fullName split into two seperate elements in an array.
-// (In other words, the variable fullName is equal to "Linus Torvalds", then splitName should equal ["Linus", "Torvalds"].)
-// Print splitName to the console.
-// HINT: Remember to research the methods and concepts listed in the instructions PDF.
+/*
+4) Declare a variable named splitName and set it equal to fullName split into two seperate elements in an array. (In other words, the variable fullName is equal to "Linus Torvalds", then splitName should equal ["Linus", "Torvalds"].) Print splitName to the console to test your result. HINT: Remember to research the methods and concepts listed in the instructions PDF.
+*/
 
 
 
 
-// 5) Write another function called "sayName" that takes no parameters.
-// When called, this function should return "Hello, [name]!", where the name is equal to the first value in the splitName array from #4.
-// Call the function.
+/*
+5) Write another function called "sayName" that takes no parameters. When called, this function should return "Hello, [name]!", where the name is equal to the first value in the splitName array from #4. Call the function.
+*/
 
 
 
 
-// 6) Write another function named linusAge.  This function should take one parameter: the year Linus was born, and it should return the implied age.
-// Call the function, passing the year Linus was born as the argument/parameter.
-// HINT: http://www.w3schools.com/js/js_functions.asp
+/*
+6) Write another function named linusAge.  This function should take one parameter: the year Linus was born, and it should return the implied age. Call the function, passing the year Linus was born as the argument/parameter. HINT: http://www.w3schools.com/js/js_functions.asp
+*/
 
 
 
 
-// 7) Using the basic function given below, add code to return the sum of all the odd numbers from 1 to 5000.  Don't forget to call the function!
-// HINT: Consider using a 'for loop'.
+/*
+7) Using the basic function given below, add code to return the sum of all the odd numbers from 1 to 5000.  Don't forget to call the function! HINT: Consider using a 'for loop'.
 
 function sum_odd_numbers() {
     var sum = 0;
@@ -90,19 +73,13 @@ function sum_odd_numbers() {
     // Write your code here
 
 
-
     return sum;
 }
+*/
 
 
 
-
-// That's it! After you're satisfied, click "Save & Continue"
-
-//////////////////////////////////////////////////////////////////
-/////////////////////////////THE END!/////////////////////////////
-//////////////////////////////////////////////////////////////////
-    `;
+// That's it! After you're satisfied, click "Save & Continue"`;
 
     if (localStorage.getItem('code')) {
       $scope.code = localStorage.getItem('code');
